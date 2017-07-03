e41cdc8f1c4da9e5610f2762b8c3573441d74d02
DJ_Kovrik <djkovrik@gmail.com>
2017-07-03 16:43:06 +0300

    Fix retrofit request urls

--- app/src/main/java/com/sedsoftware/comicser/data/source/remote/ComicVineService.java
+++ app/src/main/java/com/sedsoftware/comicser/data/source/remote/ComicVineService.java
@@ -16,40 +16,40 @@ import retrofit2.http.QueryMap;
 
 public interface ComicVineService {
 
-  String ENDPOINT = "https://comicvine.gamespot.com/api";
+  String ENDPOINT = "https://comicvine.gamespot.com/";
   String ISSUE_TYPE_CODE = "4000";
   String VOLUME_TYPE_CODE = "4050";
   String CHARACTER_TYPE_CODE = "4005";
 
   // Request issues list
-  @GET("/issues")
+  @GET("/api/issues/")
   Observable<ServerResponse<List<ComicIssueInfoList>>> getIssuesList(
       @QueryMap Map<String, String> options);
 
   // Request issue details
-  @GET("/issue/" + ISSUE_TYPE_CODE + "-{id}")
+  @GET("/api/issue/" + ISSUE_TYPE_CODE + "-{id}/")
   Observable<ServerResponse<ComicIssueInfo>> getIssueDetails(
       @Path("id") long issueId,
       @QueryMap Map<String, String> options);
 
   // Request volumes list
-  @GET("/volumes")
+  @GET("/api/volumes/")
   Observable<ServerResponse<List<ComicVolumeInfoList>>> getVolumesList(
       @QueryMap Map<String, String> options);
 
   // Request volume details
-  @GET("/volume/" + VOLUME_TYPE_CODE + "-{id}")
+  @GET("/api/volume/" + VOLUME_TYPE_CODE + "-{id}/")
   Observable<ServerResponse<ComicVolumeInfo>> getVolumeDetails(
       @Path("id") long volumeId,
       @QueryMap Map<String, String> options);
 
   // Request characters list
-  @GET("/characters")
+  @GET("/api/characters/")
   Observable<ServerResponse<List<ComicCharacterInfoList>>> getCharactersList(
       @QueryMap Map<String, String> options);
 
   // Request character details
-  @GET("/character/" + CHARACTER_TYPE_CODE + "-{id}")
+  @GET("/api/character/" + CHARACTER_TYPE_CODE + "-{id}/")
   Observable<ServerResponse<ComicCharacterInfo>> getCharacterDetails(
       @Path("id") long characterId,
       @QueryMap Map<String, String> options);
