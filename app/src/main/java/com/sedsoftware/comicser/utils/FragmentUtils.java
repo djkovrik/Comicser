740680edd972a2cde0c6e1b427a90f9c303a8684
DJ_Kovrik <djkovrik@gmail.com>
2017-07-04 12:10:01 +0300

    Add fragment utils

---
+++ app/src/main/java/com/sedsoftware/comicser/utils/FragmentUtils.java
@@ -0,0 +1,23 @@
+package com.sedsoftware.comicser.utils;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class FragmentUtils {
+
+  public static void addFragmentTo(@NonNull FragmentManager fragmentManager,
+      @NonNull Fragment fragment, int frameId) {
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    transaction.add(frameId, fragment);
+    transaction.commit();
+  }
+
+  public static void replaceFragmentIn(@NonNull FragmentManager fragmentManager,
+      @NonNull Fragment fragment, int frameId) {
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    transaction.replace(frameId, fragment);
+    transaction.commit();
+  }
+}
