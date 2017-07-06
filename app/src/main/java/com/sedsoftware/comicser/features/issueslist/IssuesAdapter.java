8a25132142e0f8ea83a3f94d578e337ea59865d2
DJ_Kovrik <djkovrik@gmail.com>
2017-07-06 21:07:19 +0300

    Add image placeholders for Glide

--- app/src/main/java/com/sedsoftware/comicser/features/issueslist/IssuesAdapter.java
+++ app/src/main/java/com/sedsoftware/comicser/features/issueslist/IssuesAdapter.java
@@ -6,11 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.sedsoftware.comicser.R;
 import com.sedsoftware.comicser.data.model.ComicIssueInfoList;
 import com.sedsoftware.comicser.features.issueslist.IssuesAdapter.IssueViewHolder;
@@ -63,6 +67,8 @@ class IssuesAdapter extends RecyclerView.Adapter<IssueViewHolder> {
     ImageView issueCover;
     @BindView(R.id.issue_name)
     TextView issueName;
+    @BindView(R.id.issue_cover_progressbar)
+    ProgressBar progressBar;
 
     IssueViewHolder(View itemView) {
       super(itemView);
@@ -86,6 +92,24 @@ class IssuesAdapter extends RecyclerView.Adapter<IssueViewHolder> {
         Glide.with(issueCover.getContext())
             .load(url)
             .fitCenter()
+            .crossFade()
+            .listener(new RequestListener<String, GlideDrawable>() {
+              @Override
+              public boolean onException(Exception e, String model, Target<GlideDrawable> target,
+                  boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+              }
+
+              @Override
+              public boolean onResourceReady(GlideDrawable resource, String model,
+                  Target<GlideDrawable> target,
+                  boolean isFromMemoryCache, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+              }
+            })
+            .error(R.drawable.placeholder_error)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(issueCover);
       }
